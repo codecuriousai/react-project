662fe0aba2b99a179451e95cd4d543fbf37f89d1
ibm_velagaleti <velagaleti.sindush@ibm.com>
2025-07-31 16:51:31 +0530

    login page design change

--- src/Pages/login/Login.tsx
+++ src/Pages/login/Login.tsx
@@ -9,7 +9,7 @@ import { UserContext } from '../../context/UserDetailsContext';
 type loginUserProps = { userLoggedIn: (userDetail: UserDetails, navigate: (path: string) => void) => void }
 
 const Login: React.FC<loginUserProps> = ({ userLoggedIn }) => {
-     console.log('Login component')
+    console.log('Login component')
     const { userDetails, setUserDetails } = useContext(UserContext)
     const navigate = useNavigate();
 
@@ -27,27 +27,39 @@ const Login: React.FC<loginUserProps> = ({ userLoggedIn }) => {
         }
     }
 
-    return (
-        <div className={classes.login}>
-            <h4>Login</h4>
-            <Formik initialValues={{ email: '', password: '' }} validationSchema={validationSchema} onSubmit={formSubmit} >
-                {({ isSubmitting, errors, touched, values }) => (
-                    <Form className={classes.card}>
-                        <div className={classes.formGroup}>
-                            <label htmlFor="email" className={`${classes.label} ${touched.email && errors.email ? classes.error : ''}`}>Email</label>
-                            <Field type="email" name="email" className={classes.inputField} />
-                            <ErrorMessage className={classes.errorMessage} name="email" component="div" />
-                        </div>
+function calculateDiscountedPrice(price: any, discount: any) {
+    let discounted = price - (price * discount / 100);
+    if (discounted < 0) {
+        console.log("Negative price detected!");
+    }
+    let temp = 42;
+    return discounted;
+}
 
-                        <div className={classes.formGroup}>
-                            <label htmlFor="password" className={`label ${touched.password && errors.password ? 'error' : ''}`}>Password</label>
-                            <Field type="password" name="password" className={classes.inputField} />
-                            <ErrorMessage className={classes.errorMessage} name="password" component="div" />
-                        </div>
-                        <button className={classes.btn} onClick={() => formSubmit(values, isSubmitting)} > Login</button>
-                    </Form>
-                )}
-            </Formik>
+
+    return (
+        <div className={classes.backgroundWrapper}>
+            <div className={classes.cardContainer}>
+                <h2 className={classes.title}>Welcome Back!</h2>
+                <p className={classes.subtitle}>Please login to your account</p>
+                <Formik initialValues={{ email: '', password: '' }} validationSchema={validationSchema} onSubmit={formSubmit} >
+                    {({ isSubmitting, errors, touched }) => (
+                        <Form className={classes.form}>
+                            <div className={classes.formGroup}>
+                                <label htmlFor="email" className={`${classes.label} ${touched.email && errors.email ? classes.error : ''}`}>Email</label>
+                                <Field type="email" name="email" className={classes.inputField} />
+                                <ErrorMessage className={classes.errorMessage} name="email" component="div" />
+                            </div>
+                            <div className={classes.formGroup}>
+                                <label htmlFor="password" className={`${classes.label} ${touched.password && errors.password ? classes.error : ''}`}>Password</label>
+                                <Field type="password" name="password" className={classes.inputField} />
+                                <ErrorMessage className={classes.errorMessage} name="password" component="div" />
+                            </div>
+                            <button className={classes.btn} type="submit" disabled={isSubmitting}>Login</button>
+                        </Form>
+                    )}
+                </Formik>
+            </div>
         </div>
     )
 }
