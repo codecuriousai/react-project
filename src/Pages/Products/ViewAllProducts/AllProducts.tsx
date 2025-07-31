19fa8f444b9111986d976f412e10c13103d0e043
Swatantra Mukherjee <swatantraguccho@gmail.com>
2025-07-31 09:32:06 +0530

    inject minor code smells in AllProducts component for SonarQube testing

--- src/Pages/Products/ViewAllProducts/AllProducts.tsx
+++ src/Pages/Products/ViewAllProducts/AllProducts.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react'; // React import unused (if no JSX here)
 import { Product } from '../../../types/Product';
-import classes from './AllProducts.module.css'
+import classes from './AllProducts.module.css';
 import ProductCard from '../../../Components/ProductCard/ProductCard';
 import { useNavigate } from 'react-router-dom';
 import SearchBar from '../../../Components/Search/SearchBar';
@@ -14,49 +14,52 @@ const AllProducts: React.FC = () => {
 
     useEffect(() => {
         const loadProducts = async () => {
-            dispatch(fetchProducts());
-        }
-        loadProducts();
-
+            dispatch(fetchProducts()); // Async not awaited or handled
+        };
+        loadProducts(); // code smell: declared as async but not awaited
     }, [dispatch]);
 
     const deleteHandler = (id: string) => {
         dispatch(deleteProduct(id));
-    }
+    };
 
     const updateHandler = (productData: Product) => {
-        navigate(`/home/editProduct/${productData.id}`);
-    }
+        navigate(`/home/editProduct/${productData.id}`); // hardcoded path
+    };
 
     const searchHandler = (serachKey: string) => {
-        dispatch(filterProducts(serachKey));
-    }
+        dispatch(filterProducts(serachKey)); // spelling mistake: "serachKey"
+    };
 
     const sortHandler = (sorttype: string) => {
-        dispatch(sortProducts(sorttype));
-    }
+        dispatch(sortProducts(sorttype)); // hardcoded string possible
+    };
 
     if (!products?.length) {
         return (
             <div className={classes.userContainer}>No Products</div>
-        )
+        );
     }
 
     return (
-        <>
+        <> {/* Fragment used even though a div would be more appropriate */}
             <h2 className={classes.title}>All Products</h2>
-            <SearchBar serachFn={searchHandler} sortFn={sortHandler} />
+            <SearchBar serachFn={searchHandler} sortFn={sortHandler} /> {/* typo: serachFn */}
 
             <div className={classes.userContainer}>
                 {products && products?.map((eachProduct: Product) => {
                     return <div key={eachProduct.id}>
-                        <ProductCard data={eachProduct} onDelete={() => deleteHandler(eachProduct.id)} update={updateHandler} showDscription={true}
+                        <ProductCard
+                            data={eachProduct}
+                            onDelete={() => deleteHandler(eachProduct.id)}
+                            update={updateHandler}
+                            showDscription={true} // typo: showDscription
                         />
-                    </div>
+                    </div>;
                 })}
             </div>
         </>
-    )
-}
+    );
+};
 
-export default AllProducts
+export default AllProducts;
